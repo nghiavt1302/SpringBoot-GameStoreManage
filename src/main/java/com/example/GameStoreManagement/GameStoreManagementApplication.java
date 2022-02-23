fea2b08c06247b4880903b7963e0365c4e0372b5
nghiavt <vtnghia01.bchkl@gmail.com>
2022-02-23 18:01:41 +0700

    Create project

---
+++ src/main/java/com/example/GameStoreManagement/GameStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.GameStoreManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GameStoreManagement/GameStoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GameStoreManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
